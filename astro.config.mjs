de2378c51800dd56df0a82c887120fb46c957b6d
Enric Cabeza Perez <91900904+enrikku@users.noreply.github.com>
2025-03-28 21:13:09 +0100

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,14 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
       plugins: [tailwindcss()]
 	},
+  adapter: vercel({}),
   output: 'server',
   integrations: [react()]
 });
