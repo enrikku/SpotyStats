4af3fe637adedd3532575f7b5d1faed964cf095c
enrikku <enric.cancru@gmail.com>
2025-11-19 18:56:41 +0100

    Add middleware to redirect authenticated users
    Introduces a middleware that checks for an 'accessToken' cookie on the root path ('/'). If present, the user is redirected to '/home'; otherwise, the request proceeds as normal.

---
+++ src/middleware.ts
@@ -0,0 +1,12 @@
+export function onRequest(context: any, next: any) {
+  const cookie = context.cookies.get("accessToken")?.value;
+  
+  if (context.url.pathname === "/") {
+    if (cookie) {
+      const redirectUrl = new URL("/home", context.url);
+      return Response.redirect(redirectUrl.toString(), 302);
+    }
+  }
+
+  return next();
+}
